b21f2f3f95b6a5f219cb78f6a824c87e31682a1c
Aya <squallihoussainiaya@gmail.com>
2025-04-14 12:40:00 +0100

    configure router

---
+++ src/views/Quiz.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="quiz">
+    <h1>Quiz</h1>
+    <router-link to="/" class="btn">Back to Home</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'QuizView'
+}
+</script>
+
+<style scoped>
+.quiz {
+  text-align: center;
+  padding: 2rem;
+}
+
+.btn {
+  display: inline-block;
+  padding: 0.5rem 1rem;
+  background-color: #42b983;
+  color: white;
+  text-decoration: none;
+  border-radius: 4px;
+  margin-top: 1rem;
+}
+
+.btn:hover {
+  background-color: #3aa876;
+}
+</style> 
