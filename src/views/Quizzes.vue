10cc9b6a42f3106b6fa0de339887123773c725c0
Assia snissi <assia.snissi@um6p.ma>
2025-04-17 23:44:00 +0100

    quizzes created

--- src/views/Quizzes.vue
+++ src/views/Quizzes.vue
@@ -1,23 +1,57 @@
 <template>
-    <div>
-        <p>{{Quiz.id}}{{ Quiz.name }} {{ Quiz.topic }}</p>
-        <router-link :to= '/take-quiz/'>TAKE THE QUIZ</router-link>
+  <div >
+    <h1>All Quizzes</h1>
+    <div v-if="loading">Loading quizzes...</div>
+    <div v-else>
+      <div v-if="quizzes.length === 0">No quizzes found.</div>
+      <div v-else >
+        <div
+          v-for="quiz in quizzes" :key="quiz.id">
+          <h2>{{ quiz.title }}</h2>
+          <p><strong>Author:</strong> {{ quiz.author }}</p>
+          <p><strong>Topic:</strong> {{ quiz.topic }}</p>
+          <div class="mt-2">
+            <h3 class="font-medium">Questions:</h3>
+            <ul class="list-disc pl-6">
+              <li v-for="(q, i) in quiz.questions" :key="i">
+                {{ q.type === 'qcm' ? '[QCM]' : '[T/F]' }} {{ q.text }}  
+                <br/>
+              </li>
+            </ul>
+          </div>
+          <router-link :to="`/take-quiz/${quiz.id}`">
+            <button>Take Quiz</button>
+          </router-link>
+        </div>
+      </div>
     </div>
-    
+  </div>
 </template>
 
-
 <script>
-//kayn wa7d id manquant f route dyal dak lbutton 
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../firebase-config.js"; 
 
 export default {
-  name: 'Quizzes',
-  components: {
-   
-  }
-}
+  name: "Quizzes",
+  data() {
+    return {
+      quizzes: [],
+      loading: true,
+    };
+  },
+  async created() {
+    try {
+      const querySnapshot = await getDocs(collection(db, "quizzes"));
+      this.quizzes = querySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+    } catch (error) {
+      console.error("Error fetching quizzes:", error);
+    } finally {
+      this.loading = false;
+    }
+  },
+};
 </script>
-
-<style scoped>
-
-</style>
