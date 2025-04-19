5c2a8372fc8b45b068aaa9e369c383932ec9ed82
Rhita-Rh <rhitarhallami02@gmail.com>
2025-04-19 12:39:45 +0100

    commit maroua

--- src/components/myquizzes.vue
+++ src/components/myquizzes.vue
@@ -1,10 +1,12 @@
 <template>
   <div>
+    <h3>My Quizzes</h3>
     <div v-if="quizzes.length === 0">You haven't created any quizzes yet.</div>
-    <div v-for="quiz in quizzes" :key="quiz.id">
-      <h4>{{ quiz.title }}</h4>
-      <p>Topic: {{ quiz.topic }}</p>
-    </div>
+    <ul>
+      <li v-for="quiz in quizzes" :key="quiz.id">
+        {{ quiz.title }}
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -22,8 +24,10 @@ export default {
   async mounted() {
     const user = auth.currentUser;
     if (!user) return;
+
     const q = query(collection(db, 'quizzes'), where('ownerId', '==', user.uid));
     const querySnapshot = await getDocs(q);
+
     this.quizzes = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
   }
 };
