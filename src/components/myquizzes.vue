45058d1d41e70e0cbe1f3d6b646527dc5d267286
SniAssia <assia.snissi@um6p.ma>
2025-04-19 12:02:12 +0000

    update editquiz and myquizzes

--- src/components/myquizzes.vue
+++ src/components/myquizzes.vue
@@ -4,16 +4,17 @@
     <div v-for="quiz in quizzes" :key="quiz.id">
       <h4>{{ quiz.title }}</h4>
       <p>Topic: {{ quiz.topic }}</p>
+      <button @click="goToEditQuiz(quiz.id)">Edit</button>
     </div>
   </div>
 </template>
 
 <script>
-import { db, auth } from '../firebase';
+import { db, auth } from '../firebase-config';
 import { collection, query, where, getDocs } from 'firebase/firestore';
 
 export default {
-  name: 'myquizzes',
+  name: 'MyQuizzes',
   data() {
     return {
       quizzes: []
@@ -25,6 +26,11 @@ export default {
     const q = query(collection(db, 'quizzes'), where('ownerId', '==', user.uid));
     const querySnapshot = await getDocs(q);
     this.quizzes = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+  },
+  methods: {
+    goToEditQuiz(quizId) {
+      this.$router.push({ name: 'EditQuiz', params: { quizId } });
+    }
   }
 };
 </script>
