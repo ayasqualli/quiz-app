ca4ae480441d5e71ccb7867a1ad2b7929a4337b7
Aya <squallihoussainiaya@gmail.com>
2025-04-21 15:06:47 +0100

    add analytics for the app

--- src/main.js
+++ src/main.js
@@ -1,9 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import { inject } from "@vercel/analytics"
-
-// Initialize Vercel analytics
-inject()
 
 createApp(App).use(router).mount('#app')
